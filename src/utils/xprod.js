9f3457e434b814632d29ea2c20ba39de18fbb2bd
Hendrik Mittrop <hendrik.mittrop@zalando.de>
2018-05-31 13:56:23 +0200

    add xprod helper function

---
+++ src/utils/xprod.js
@@ -0,0 +1,13 @@
+const xprod = (aList = [], bList = []) => {
+  const res = []
+
+  aList.forEach(aItem => {
+    bList.forEach(bItem => {
+      res.push([aItem, bItem])
+    })
+  })
+
+  return res
+}
+
+export default xprod
